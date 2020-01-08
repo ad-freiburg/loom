773c0acd0a1d9a1afa8da858de03656034d75d1c
Patrick Brosi <brosi@informatik.uni-freiburg.de>
2020-01-08 23:51:55 +0100

    changes

---
+++ src/transitmap/output/Renderer.h
@@ -0,0 +1,23 @@
+// Copyright 2016, University of Freiburg,
+// Chair of Algorithms and Data Structures.
+// Authors: Patrick Brosi <brosi@informatik.uni-freiburg.de>
+
+#ifndef TRANSITMAP_OUTPUT_RENDERER_H_
+#define TRANSITMAP_OUTPUT_RENDERER_H_
+
+#include "transitmap/graph/TransitGraph.h"
+
+namespace transitmapper {
+namespace output {
+
+class Renderer {
+ public:
+  virtual ~Renderer() {};
+
+  // print the outputGraph
+  virtual void print(const graph::TransitGraph& outputGraph) = 0;
+};
+
+}}
+
+#endif  // TRANSITMAP_OUTPUT_RENDERER_H_
