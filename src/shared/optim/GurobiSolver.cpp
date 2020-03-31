d7e9e244beef7afff142f5b08a89f48e914ac445
Patrick Brosi <brosi@informatik.uni-freiburg.de>
2020-03-31 12:14:36 +0200

    changes

--- src/shared/optim/GurobiSolver.cpp
+++ src/shared/optim/GurobiSolver.cpp
@@ -200,6 +200,9 @@ SolveType GurobiSolver::solve() {
     }
   }
 
+  GRBsetintparam(GRBgetenv(_model), GRB_INT_PAR_PRESOLVE, 2);
+
+
   // 10 sec tuning
   // GRBsetdblparam(GRBgetenv(_model), GRB_DBL_PAR_TUNETIMELIMIT, 10);
 
