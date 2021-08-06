f51b1ba5d68bc434221ddc4de1bbd9813664596c
Patrick Brosi <brosi@informatik.uni-freiburg.de>
2021-08-06 18:15:55 +0200

    changes

--- src/loom/tests/TestMain.cpp
+++ src/loom/tests/TestMain.cpp
@@ -2,50 +2,74 @@
 // Author: Patrick Brosi
 //
 
-#include "shared/rendergraph/RenderGraph.h"
-#include "loom/optim/CombOptimizer.h"
+#include <vector>
 #include "loom/config/LoomConfig.h"
+#include "loom/optim/CombOptimizer.h"
+#include "shared/rendergraph/RenderGraph.h"
+
+struct FileTest {
+  std::string fname;
+  size_t sameSegCrossings, diffSegCrossings, separations;
+};
+
+static const std::vector<FileTest> fileTests(
+    {{"/home/patrick/repos/loom/src/loom/tests/datasets/y-splitting.json", 0, 0,
+      0}});
 
 // _____________________________________________________________________________
 int main(int argc, char** argv) {
   UNUSED(argc);
   UNUSED(argv);
 
-  loom::config::Config cfg;
-  cfg.untangleGraph = false;
-  cfg.collapseLinePartners = false;
-  cfg.separationOpt = false;
-  cfg.createCoreOptimGraph = false;
-  cfg.optimRuns = 1;
+  loom::config::Config baseCfg;
+  baseCfg.untangleGraph = false;
+  baseCfg.collapseLinePartners = false;
+  baseCfg.separationOpt = false;
+  baseCfg.createCoreOptimGraph = false;
+  baseCfg.optimRuns = 1;
 
   shared::rendergraph::Penalties pens{1, 1, 1, 1, 1, 1, 1, 1, false, false};
+
+  std::vector<loom::config::Config> configs;
+  configs.push_back(baseCfg);
+
+  baseCfg.collapseLinePartners = true;
+  baseCfg.createCoreOptimGraph = true;
+  configs.push_back(baseCfg);
+
+  baseCfg.untangleGraph = true;
+  configs.push_back(baseCfg);
 
   // TODO
   // TEST(g.numEdgs() == 18);
   // TEST(g.numNds() == 18);
 
-  loom::optim::ExhaustiveOptimizer exhausOptim(&cfg, pens);
-  loom::optim::ILPOptimizer ilpOptim(&cfg, pens);
-  loom::optim::ILPEdgeOrderOptimizer ilpImprOptim(&cfg, pens);
-  loom::optim::CombOptimizer combOptim(&cfg, pens);
+  for (const auto& cfg : configs) {
+    loom::optim::ExhaustiveOptimizer exhausOptim(&cfg, pens);
+    loom::optim::ILPOptimizer ilpOptim(&cfg, pens);
+    loom::optim::ILPEdgeOrderOptimizer ilpImprOptim(&cfg, pens);
+    loom::optim::CombOptimizer combOptim(&cfg, pens);
 
-  std::vector<loom::optim::Optimizer*> optimizers;
-  optimizers.push_back(&exhausOptim);
-  optimizers.push_back(&ilpOptim);
-  optimizers.push_back(&ilpImprOptim);
-  optimizers.push_back(&combOptim);
+    std::vector<loom::optim::Optimizer*> optimizers;
+    optimizers.push_back(&exhausOptim);
+    optimizers.push_back(&ilpOptim);
+    optimizers.push_back(&ilpImprOptim);
+    optimizers.push_back(&combOptim);
 
-  for (auto optim : optimizers) {
-    shared::rendergraph::RenderGraph g(5, 5);
+    for (auto optim : optimizers) {
+      for (const auto& test : fileTests) {
+        shared::rendergraph::RenderGraph g(5, 5);
 
-    std::ifstream input;
-    input.open("/home/patrick/repos/loom/src/loom/tests/datasets/y-splitting.json");
-    g.readFromJson(&input, 3);
+        std::ifstream input;
+        input.open(test.fname);
+        g.readFromJson(&input, 3);
 
-    auto res = optim->optimize(&g);
+        auto res = optim->optimize(&g);
 
-    TEST(res.sameSegCrossings, ==, 0);
-    TEST(res.diffSegCrossings, ==, 0);
-    TEST(res.separations, ==, 0);
+        TEST(res.sameSegCrossings, ==, test.sameSegCrossings);
+        TEST(res.diffSegCrossings, ==, test.diffSegCrossings);
+        TEST(res.separations, ==, test.separations);
+      }
+    }
   }
 }
